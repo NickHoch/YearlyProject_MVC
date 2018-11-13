100ddfca785410f993eb0e449005a889ee3b94bf
NickHoch <NickHoch@users.noreply.github.com>
2018-11-13 23:51:02 +0200

    not important change

--- Home3__MVC/Scripts/calcSum.js
+++ Home3__MVC/Scripts/calcSum.js
@@ -4,10 +4,4 @@
         sum += parseFloat($(this).html());
     });
     $('#sumBucket').html("Total sum: " + sum + "uah");
-    //$('#bucketTb').on('update', function () {
-    //    $('#ordItemBd td:nth-last-of-type(2)').each(function () {
-    //        sum += parseFloat($(this).html());
-    //    });
-    //    $('#sumBucket').html("Total sum: " + sum);
-    //});
 });
